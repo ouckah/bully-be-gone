2cced3b2152c74fccd59854b4e249dc6bfa52c6e
Aidan <datbluemushroom@gmail.com>
2022-10-01 20:40:50 -0400

    🚀 Code for Taki and Marcus

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
     <>
-      <div className='flex justify-center items-center w-full h-screen'>
-        <h1 className='font-bold text-5xl'>Hi!</h1>
-      </div>
+      <>
+      <h1>Home Page</h1>
+      <Link href='/admin'>
+        <div className="w-full h-20 bg-white"></div>
+      </Link>
+    </>
     </>
   )
 }
