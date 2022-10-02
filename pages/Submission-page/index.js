30dea8f9a66c102f3696d4ed32331e5d9de4a214
Marcus <mjhom17@gmail.com>
2022-10-01 20:32:03 -0400

    Create index.js

---
+++ pages/Submission-page/index.js
@@ -0,0 +1,10 @@
+const Submission = () =>
+{
+    return (
+        <div>
+            <hi> This is the page to let you know you submitted a report </hi>
+        </div>
+    )
+}
+
+export default Submission 
